51bb89cf37abb61b0737df45ca9adf8cae10a3a5
txd22081999 <txd22081999@gmail.com>
2021-01-22 17:55:32 +0700

    add some change

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import './App.scss';
-import Header from './components/Header';
-import LeftBar from './components/LeftBar';
-import RightBar from './components/RightBar';
-import Core from './components/Core';
-import Login from './components/Login';
+import "./App.scss";
+import Header from "./components/Header";
+import LeftBar from "./components/LeftBar";
+import RightBar from "./components/RightBar";
+import Core from "./components/Core";
+import Login from "./components/Login";
+
+import { selectUser } from "./features/userSlice";
+import { useSelector } from "react-redux";
 
 function App() {
+  const user = useSelector((state) => state.user);
+
+  console.log(user);
+
   return (
-    <div className='App'>
+    <div className="App">
       <Header />
       {/* <main>
         <LeftBar />
